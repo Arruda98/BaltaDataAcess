1d4d40a15d5c9dd13a25f9d407bde24a42eafbd4
Arruda98 <jeffersonarruda98@gmail.com>
2026-02-11 22:11:01 -0300

    Estudo sobre acesso a banco de dados

--- BaltaDataAcess/Program.cs
+++ BaltaDataAcess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 
 namespace BaltaDataAcess
 {
@@ -6,9 +7,28 @@ namespace BaltaDataAcess
     {
         static void Main(string[] args)
         {
-            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
-            
-            Console.WriteLine("");
+            const string connectionString =
+            "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;" +
+            "Encrypt=True;TrustServerCertificate=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Conectado");
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
