de54148331787fb33314bdb7d3478b06159a9e07
Arruda98 <jeffersonarruda98@gmail.com>
2026-02-11 21:20:25 -0300

    Adicionar arquivos de projeto.

---
+++ BaltaDataAcess/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BaltaDataAcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+            
+            Console.WriteLine("");
+        }
+    }
+}
